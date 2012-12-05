96998713c826cc8ab7db2b646443e1a0438d466a
fimbrethil <fimbrethil.wandlimb@gmail.com>
2012-12-05 01:47:35 -0800

    Moved tests to test folder.

---
+++ tests/videositeTests.py
@@ -0,0 +1,33 @@
+'''
+Created on Nov 27, 2012
+
+@author: christian
+'''
+import unittest
+from hostsite.gorillavid import GorillaVid
+from inspect import stack
+from hostsite.hostsite import HostSite
+
+
+class HostSiteTest():
+    '''
+    classdocs
+    '''
+    def stateTest(self):
+        self.failUnless(self.hs.url in self.hs.getBaseUrl())
+    def getVideoTest(self):
+        assert isinstance(self, unittest.TestCase)
+        video = self.hs
+
+#    def getBaseUrlTest(self):
+
+#class GorillaVidTest(HostSiteTest, unittest.TestCase):
+#    @classmethod
+#    def setUpClass(cls):
+#        cls.hs = GorillaVid("http://gorillavid.com/5jmfrah9alxt")
+#        print "in " + cls.__name__ + " passed " + str(stack()[0][3])
+
+
+#if __name__ == "__main__":
+#    #import sys;sys.argv = ['', 'Test.testName']
+#    unittest.main()
