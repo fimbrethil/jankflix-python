ab6b0d844d3360e5ae65df4b47ee9c6e269d9279
fimbrethil <fimbrethil.wandlimb@gmail.com>
2012-12-05 01:49:11 -0800

    Added setup file to make this project easy to install.

---
+++ setup.py
@@ -0,0 +1,24 @@
+'''
+Created on Dec 3, 2012
+
+@author: christian
+'''
+from setuptools import setup, find_packages
+for m in ('BeautifulSoup', 'urllib', 'urllib2', 'urlparse'):
+    try:
+        __import__(m)
+    except ImportError:
+        pass
+install_requires = [
+    'BeautifulSoup',
+]
+
+setup(name = 'jankflix',
+      version = '.9',
+      packages = find_packages(exclude = ("tests",)),
+      install_requires = install_requires,
+      entry_points = {
+        'console_scripts': [
+            'jankflix = scripts.cli:main'
+        ]}
+      )
