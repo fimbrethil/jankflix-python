20fd750355808d59819cd01fc250ce959bcb0e37
fimbrethil <fimbrethil.wandlimb@gmail.com>
2012-12-05 01:36:05 -0800

    Added functionality to test many host sites on a page.
    This will help test that all of the video sites still work.

--- tests/hostsitetest.py
+++ tests/hostsitetest.py
@@ -6,25 +6,56 @@ Created on Nov 27, 2012
 import unittest
 from hostsite.gorillavid import GorillaVid
 from inspect import stack
+from linksite.tvlinks import TVLinks
+from linksite.onechannel import OneChannel
+from linksite import linksite
+from linksite.linksite import LinkSite
+from hostsite.hostsite import HostSite
 
+#
+#class HostSiteTest():
+#
+#    def testGetVideo(self):
+#
+#        video = self.hs.getVideo()
+#        print type(video)
+#        self.failUnless(type(video) is str)
+#        print video
+#    def testGetBaseUrl(self):
+#        print self.hs.url, self.hs.getBaseUrl()
+#        self.failUnless(self.hs.getBaseUrl() in self.hs.url)
+#
+#class GorillaVidTest(HostSiteTest, unittest.TestCase):
+#    @classmethod
+#    def setUpClass(cls):
+#        cls.hs = GorillaVid("http://gorillavid.com/5jmfrah9alxt")
+#        print "in " + cls.__name__ + " passed " + str(stack()[0][3])
 
-class HostSiteTest():
-
-    def testGetVideo(self):
-        video = self.hs.getVideo()
-        self.failUnless(type(video) is str)
-        print video
-    def testGetBaseUrl(self):
-        print self.hs.url, self.hs.getBaseUrl()
-        self.failUnless(self.hs.url in self.hs.getBaseUrl())
-
-class GorillaVidTest(HostSiteTest, unittest.TestCase):
+class TestManyHostSites(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.hs = GorillaVid("http://gorillavid.com/5jmfrah9alxt")
+        search = TVLinks.searchSite("avatar")
+        l = None
+        for name, link in search:
+            if "Airbender" in name :
+                l = link
+        print l
+        cls.tl = TVLinks(l)
         print "in " + cls.__name__ + " passed " + str(stack()[0][3])
-
-
+    def testManyHostSites(self):
+        season = 2
+        episode = 3
+        sites = self.tl.getHostSiteTypes(season, episode)
+        for i in range(len(sites)):
+            site = sites[i]
+            for name, hs in linksite.supportedSites:
+                if name in site:
+                    linksite.supportedSites.remove((name, hs))
+                    hostsiteurl = self.tl.getHostSiteAtIndex(season, episode, i)
+                    assert isinstance(hs, HostSite)
+                    hs.__init__(hostsiteurl)
+                    print hs.getVideo()
+                    print "got video for " + name
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
