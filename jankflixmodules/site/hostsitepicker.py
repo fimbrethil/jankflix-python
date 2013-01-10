04dca0d486ee39382ba389b0b1d8d6f13b6bc6fb
fimbrethil <fimbrethil.wandlimb@gmail.com>
2013-01-10 10:30:12 -0800

    Hostsitepicker revamp.
    Upon encountering a bad hostsite, it will fall back on another
    one making the whole program much more robust.

--- jankflixmodules/site/hostsitepicker.py
+++ jankflixmodules/site/hostsitepicker.py
@@ -5,23 +5,40 @@ def allSubclasses(cls):
     return cls.__subclasses__() + [g for s in cls.__subclasses__()
                                    for g in allSubclasses(s)]
     
-def pickHostSite(host_site_types):
-    for host_site_type in host_site_types:
-        for host_site in allSubclasses(HostSite):
-            if host_site.getName() in host_site_type:
-                return host_site_type,host_site
+def isSupportedHostSite(host_site_type):
+    for host_site in allSubclasses(HostSite):
+        if host_site.getName() in host_site_type:
+            return host_site
 
-def pickFromLinkSites(link_sites,season,episode):
-    for link_site in link_sites:
-        assert isinstance(link_site, LinkSite)
-        types = []
-        types = link_site.getHostSiteTypes(season, episode)
-        result = pickHostSite(types)
-        if result:
-            picked_type,picked_site = result
-            index = types.index(picked_type)
-            host_at_index = link_site.getHostSiteAtIndex(season, episode, index)
-            host_instance = picked_site(host_at_index)
-            assert isinstance(host_instance, HostSite)
-            return host_instance
-
+def pickFromLinkSite(link_site,season,episode):
+    types = link_site.getHostSiteTypes(season, episode)
+    assert isinstance(link_site, LinkSite)
+    for host_site_type in types:
+        print "trying " + host_site_type
+        host_site_object = isSupportedHostSite(host_site_type)
+        if host_site_object:
+            index = types.index(host_site_type)
+            host_at_index_link = link_site.getHostSiteAtIndex(season, episode, index)  
+            host_site_instance = host_site_object(host_at_index_link)
+            print "verifying host site"
+            if verifyGoodHostSite(host_site_instance):
+                print "hostsite good"  
+                return host_site_instance
+            else:
+                print "host site bad"
+    print "No host sites found"
+def verifyGoodHostSite(host_site):
+    print host_site.__class__
+    assert isinstance(host_site, HostSite)
+    print host_site.url
+    try:
+        videoLink = host_site.getVideo()
+        print videoLink + " is video link"
+        if len(videoLink) == 0:
+            return False
+        return True
+    except Exception as inst:
+        print type(inst)     # the exception instance
+        print inst.args      # arguments stored in .args
+        print inst     
+        return False
