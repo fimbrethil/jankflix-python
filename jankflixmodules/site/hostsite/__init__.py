749a51d9235fe464340c61d561531f10226a6bbd
fimbrethil <fimbrethil.wandlimb@gmail.com>
2013-01-27 22:53:10 -0800

    Added filenuke and filebox to what to import
    Makes them get picked up by hostsitepicker.

--- jankflixmodules/site/hostsite/__init__.py
+++ jankflixmodules/site/hostsite/__init__.py
@@ -1,4 +1,5 @@
 from jankflixmodules.site.hostsite.gorillavid import Gorillavid,Daclips,Movpod
 from jankflixmodules.site.hostsite.movreel import Movreel
 from jankflixmodules.site.hostsite.putlocker import Putlocker,Sockshare
-
+from jankflixmodules.site.hostsite.filebox import FileBox
+from jankflixmodules.site.hostsite.filenuke import FileNuke
