abd15a696d99c291744d8e691e6f8204ac3e50b0
fimbrethil <fimbrethil.wandlimb@gmail.com>
2013-01-30 02:39:01 -0800

    Daclips link broke as well as general formatting fixes

--- tests/hostsitetest.py
+++ tests/hostsitetest.py
@@ -25,13 +25,17 @@ class TestHostSite(ParametrizedTestCase):
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param=Sockshare("http://www.sockshare.com/file/0A09CBFA173FCFFA")))
+    suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = Sockshare("http://www.sockshare.com/file/0A09CBFA173FCFFA")))
     suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = Putlocker("http://www.putlocker.com/file/68EFFD1A55851B94")))
-    suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param=Movreel("http://movreel.com/vm4txj1i7m3w")))
+    
+    suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = Movreel("http://movreel.com/vm4txj1i7m3w")))
+    
     suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = Gorillavid("http://gorillavid.in/v1jy9v47gq4x")))
     suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = Gorillavid("http://gorillavid.in/n0ph9afqgr0k")))
-    suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = Daclips("http://daclips.in/q31wexpl2omp")))
+    suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = Daclips("http://daclips.in/wdl6vubb3nde")))
     suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = Movpod("http://movpod.in/aly8yr7jfw6f")))
+    
     suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = FileBox("http://www.filebox.com/pv78hy1iqqem")))
+    
     suite.addTest(ParametrizedTestCase.parametrize(TestHostSite, param = FileNuke("http://filenuke.com/3b6c44atsjky")))
     unittest.TextTestRunner(verbosity = 2).run(suite)
