cbbed985b65fcfa18dea127efc57d98f52ff27a8
fimbrethil <fimbrethil.wandlimb@gmail.com>
2013-02-04 17:02:29 -0800

    Added documentation

--- tests/newhostsitedecider.py
+++ tests/newhostsitedecider.py
@@ -2,16 +2,23 @@ from jankflixmodules.site.linksite.tvlinks import TVLinks
 from jankflixmodules.site.linksite.onechannel import OneChannel
 import operator
 def decide(show):
+    #search onechannel for show
     oc_result = OneChannel.searchSite(show)[0]
     print oc_result
-    tl_result = TVLinks.searchSite(show)[1]
+    #search tvlinks
+    tl_result = TVLinks.searchSite(show)[0]
     print tl_result
+    #instanciate both objects passing in the second part of the result tuple, the link
     oc = OneChannel(oc_result[1])
     tl = TVLinks(tl_result[1])
     all_sites = dict()
+    #season is the last season of the show
     season =oc.getSeasons()[-1]
+    #for all episodes in that season
     for episode in oc.getEpisodes(season):
         for type in oc.getHostSiteTypes(season, episode):
+            if "daclips" in type:
+                print season, episode, "onechannel"
             if type in all_sites:
                 all_sites[type] = all_sites[type] + 1
             else:
@@ -20,6 +27,8 @@ def decide(show):
     season =tl.getSeasons()[-1]
     for episode in tl.getEpisodes(season):
         for type in tl.getHostSiteTypes(season, episode):
+            if "daclips" in type:
+                print season, episode, "tvlinks"
             if type in all_sites:
                 all_sites[type] = all_sites[type] + 1
             else:
